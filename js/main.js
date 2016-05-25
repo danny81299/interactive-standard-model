75b557cbaf4138213db435ce1c5e1a3a56add0ec
danny81299 <danny81299@gmail.com>
2016-05-24 21:32:12 -0400

    added colors
    each tile has colors and corrected text alignment

--- js/main.js
+++ js/main.js
@@ -1,14 +1,16 @@
 var Particle = function (name, symbol, type, mass, spin, charge) {
+    'use strict';
+
     this.name = name;
     this.dashName = name.replace(' ', '-');
-    this.camelName = name.indexOf(' ') == -1 ? name : name.slice(0, name.indexOf(' ')) + name.charAt(name.indexOf(' ') + 1).toUpperCase() + name.slice(name.indexOf(' ') + 2, name.length);
+    this.camelName = name.indexOf(' ') === -1 ? name : name.slice(0, name.indexOf(' ')) + name.charAt(name.indexOf(' ') + 1).toUpperCase() + name.slice(name.indexOf(' ') + 2, name.length);
     this.symbol = symbol;
     this.type = type;
     this.mass = mass + 'eV/c<sup>2</sup>';
     this.spin = spin;
     this.charge = charge;
     this.el = function () {
-        return document.getElementById('smd-' + this.dashName)
+        return document.getElementById('smd-' + this.dashName);
     };
     this.focus = function () {
         $(this.el())
@@ -33,7 +35,7 @@ var Particle = function (name, symbol, type, mass, spin, charge) {
                 duration: 1000,
                 easing: "easeOutQuart"
             });
-    }
+    };
     this.blur = function () {
         $(this.el())
             .addClass('particle-blur')
@@ -45,13 +47,15 @@ var Particle = function (name, symbol, type, mass, spin, charge) {
                 duration: 1000,
                 easing: "easeOutQuart"
             });
-    }
+    };
 };
 
 var Type = function (name, type, radius, start, end, color, quant) {
+    'use strict';
+
     this.name = name;
     this.dashName = name.replace(' ', '-');
-    this.camelName = name.indexOf(' ') == -1 ? name : name.slice(0, name.indexOf(' ')) + name.charAt(name.indexOf(' ') + 1).toUpperCase() + name.slice(name.indexOf(' ') + 2, name.length);
+    this.camelName = name.indexOf(' ') === -1 ? name : name.slice(0, name.indexOf(' ')) + name.charAt(name.indexOf(' ') + 1).toUpperCase() + name.slice(name.indexOf(' ') + 2, name.length);
     this.type = type;
     this.radius = radius;
     this.start = start;
@@ -78,11 +82,11 @@ var up = new Particle('up', 'u', quark, '2.3M', '2/3', '1/2'),
     tau = new Particle('tau', '&tau;', lepton, '1.777G', '-1', '1/2'),
     electronNeutrino = new Particle('electron neutrino', '&nu;<sub>e</sub>', lepton, '2.2', '0', '1/2'),
     muonNeutrino = new Particle('muon neutrino', '&nu;<sub>&mu;</sub>', lepton, '0.17M', '0', '1/2'),
-    tauNeutrino = new Particle('tau neutrino', '&nu;<sub>&nu;</sub>', lepton, '15.5M', '0', '1/2'),
+    tauNeutrino = new Particle('tau neutrino', '&nu;<sub>&tau;</sub>', lepton, '15.5M', '0', '1/2'),
     gluon = new Particle('gluon', 'g', gaugeBoson, '0', '0', '1'),
     photon = new Particle('photon', '&gamma;', gaugeBoson, '0', '0', '1'),
     zBoson = new Particle('Z boson', 'Z', gaugeBoson, '91.2G', '0', '1'),
-    wBoson = new Particle('W boson', 'W', gaugeBoson, '80.4G', '&plusmdn;1', '1'),
+    wBoson = new Particle('W boson', 'W', gaugeBoson, '80.4G', '&plusmn;1', '1'),
     higgsBoson = new Particle('Higgs boson', 'H', higgsBosonT, '126G', '0', '0');
 
 higgsBoson.camelName = 'higgsBoson';
@@ -110,22 +114,9 @@ var windowHeight = window.innerHeight,
     windowWidth = window.innerWidth;
 
 
-function init() {
-
-    document.getElementById('standard-model-diagram').style.width = window.innerHeight + 'px';
-    document.getElementById('standard-model-diagram').style.height = window.innerHeight + 'px';
-/*    document.getElementById('smd-click-listener').style.height = window.innerHeight + 'px';
-    document.getElementById('smd-click-listener').style.width = window.innerHeight + 'px';*/
-
-    initDraw();
-
-
-    $('#smd-click-listener').click(function(event) {clickSMD(event);});
-
-
-}
-
 function initDraw() {
+    'use strict';
+
     var canvasSize = windowHeight,
         offset = canvasSize / 2,
         canvasDim = canvasSize.toString() + 'px',
@@ -151,7 +142,21 @@ function initDraw() {
          console.log(start);
          console.log(end);*/
 
-        ctx.strokeStyle = "white";
+        switch (particleType) {
+            case higgsBosonT:
+                ctx.fillStyle = '#FFEB3B';
+                break;
+            case lepton:
+                ctx.fillStyle = '#8bc34a';
+                break;
+            case gaugeBoson:
+                ctx.fillStyle = '#536dfe';
+                break;
+            case quark:
+                ctx.fillStyle = '#e91e63';
+                break;
+        }
+        ctx.strokeStyle = 'white';
         ctx.beginPath();
         if (particleType !== higgsBosonT) {
             ctx.moveTo((particleType.radius - 1) * unit * Math.cos(start) + offset, (particleType.radius - 1) * unit * Math.sin(start) + offset);
@@ -163,44 +168,68 @@ function initDraw() {
             ctx.arc(canvasSize / 2, canvasSize / 2, (particleType.radius - 1) * unit, end, start, true);
         }
         ctx.stroke();
+        ctx.fill();
 
-        ctx.fillStyle = 'white'
+        ctx.fillStyle = '#222';
+        ctx.textAlign = 'center';
         ctx.font = '1.5em Open Sans';
-        ctx.fillText(particle.name, (particleType.radius - 0.5) * unit * Math.cos((end + start) / 2) + offset, (particleType.radius - 0.5) * unit * Math.sin((end + start) / 2) + offset);
+        if (particle === higgsBoson) {
+            ctx.fillText (particle.name, offset, offset);
+        } else {
+            ctx.fillText(particle.name, (particleType.radius - 0.5) * unit * Math.cos((end + start) / 2) + offset, (particleType.radius - 0.5) * unit * Math.sin((end + start) / 2) + offset);
+        }
 
     });
 }
 
 function getParticleNum(particle) {
-
+    'use strict';
 }
 
 function focusParticle(particleNum) {
-    particles.forEach(function(particle, index) {
+    'use strict';
+
+    particles.forEach(function (particle, index) {
         if (particleNum !== index) {
             particle.blur();
         } else {
             particle.focus();
+            var data = document.getElementById('data-row'),
+                dataCells = [];
+
+            for (var i = 0; i < data.getElementsByTagName('td').length; i++) {
+                dataCells.push(data.getElementsByTagName('td')[i]);
+            }
+
+            dataCells.forEach(function (element) {
+                var dataType = element.getAttribute('data-particle-data');
+                if (dataType === "type") {
+                    element.innerHTML = particles[-1 * index + 16][dataType].name;
+                } else {
+                    element.innerHTML = particles[-1 * index + 16][dataType];
+                }
+            });
         }
     });
 }
 
 function normalizeParticles() {
-    particles.forEach(function(particle) {
-       particle.normalize();
+    'use strict';
+
+    particles.forEach(function (particle) {
+        particle.normalize();
     });
 }
 
 function getMouseParticle(x, y) {
+    'use strict';
+
     var centeredX = x - windowHeight / 2,
         centeredY = windowHeight / 2 - y,
         rawTheta = Math.atan(centeredY / centeredX),
         theta,
         radius = Math.sqrt(centeredX * centeredX + centeredY * centeredY) / (windowHeight / 8 * 1.25),
         particle = -1;
-
-    console.log(centeredX + ', ' + centeredY);
-    console.log(radius);
 
     if (centeredX === 0) {
         theta = centeredY >= 0 ? Math.PI / 2 : 3 * Math.PI / 2;
@@ -226,12 +255,28 @@ function getMouseParticle(x, y) {
 }
 
 function clickSMD(e) {
-    console.log(event.pageX + ', ' + event.pageY);
-    var part = getMouseParticle(event.pageX,event.pageY);
-    console.log(part);
+    'use strict';
+
+    var part = getMouseParticle(event.pageX, event.pageY);
     if (part !== -1) {
         focusParticle(-1 * part + 16);
     } else {
         normalizeParticles();
     }
+}
+
+function init() {
+    'use strict';
+
+    document.getElementById('standard-model-diagram').style.width = windowHeight + 'px';
+    document.getElementById('standard-model-diagram').style.height = windowHeight + 'px';
+    document.getElementById('info').style.width = (windowWidth - windowHeight).toString() + 'px';
+
+    initDraw();
+
+    $('#smd-click-listener').click(function (event) {
+        clickSMD(event);
+    });
+
+
 }
